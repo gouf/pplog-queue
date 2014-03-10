288fdca86c773f5211296e559b59ad6485105f26
gouf <innocent.zero@gmail.com>
2014-03-10 15:12:39 +0900

    deprecated メッセージに対処

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
-  config.include WebMock
+  config.include WebMock::API
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing

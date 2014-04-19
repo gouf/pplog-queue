773ea882ba2b05d5b70239fa851f1b4dec170899
gouf <innocent.zero@gmail.com>
2014-04-20 01:29:34 +0900

    dotenv を導入したので変更を戻した

--- spec/mec_spec.rb
+++ spec/mec_spec.rb
@@ -8,11 +8,11 @@ end
 describe PoemPoster do
   let(:dummyclass){Class.new {include PoemPoster}}
   context 'when load user info' do
-    it 'is user_name as "your_user_name"' do
-      expect(user_name).to eq ''
+    it 'is user_name your_user_name"' do
+      expect(user_name).to eq 'your_user_name'
     end
     it 'is password as "your_password"' do
-      expect(password).to eq ''
+      expect(password).to eq 'your_password'
     end
   end
 
